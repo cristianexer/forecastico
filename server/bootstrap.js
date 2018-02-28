67d03249b62d2581427bd980151721a743f3834a
Daniel Cristian Fat <dcristian35@yahoo.com>
2018-02-28 20:27:56 +0200

    some rights

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,8 @@
 Meteor.startup(function () {
-    if (Roles.userIsInRole(Meteor.users.findOne({ "emails.address": "dcristian35@yahoo.com" })._id,'Admin')){
+    
+    if (!Roles.userIsInRole(Meteor.users.findOne({ "emails.address": "dcristian35@yahoo.com" })._id,'Admin')){
+
         Roles.addUsersToRoles(Meteor.users.findOne({ "emails.address": "dcristian35@yahoo.com" })._id, 'Admin');
+        //fix for not giving the admin rights
     }
 });
